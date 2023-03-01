4082428c75374b1587312f9c5b59077516e20d54
Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>
2023-03-01 17:38:56 +0000

    package bumps and devcontainer tweaks

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,9 +7,6 @@ sudo make -C ~/.git-secrets install
 # cpio is required for package/release builds (npm run package)
 sudo apt -y update && sudo apt install -y cpio
 
-# Set up Dynamo
-sls dynamodb install
-
 # Some useful aliases
 # Container image includes VIM aliased as vi
 echo "alias ll='ls -alF'" >> ~/.bash_aliases
