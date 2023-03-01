8f099386d91d3ef6e4718d541f16cecf8c5f9c8e
Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>
2023-03-01 16:59:15 +0000

    adds more useful aliases

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -14,3 +14,11 @@ sls dynamodb install
 # Container image includes VIM aliased as vi
 echo "alias ll='ls -alF'" >> ~/.bash_aliases
 echo "alias vim='vi'" >> ~/.bash_aliases
+echo "alias ls='ls -lah --color=auto'" >> ~/.bash_aliases
+echo "alias ll='ls -lah --color=auto'" >> ~/.bash_aliases
+echo "alias gl='git log --pretty=\"format:%C(yellow)%h %C(green)%G?%C(auto)%d %s\" -10'" >> ~/.bash_aliases
+echo "alias gs='git status'" >> ~/.bash_aliases
+echo "alias gc='git checkout'" >> ~/.bash_aliases
+echo "alias dev='git checkout develop'" >> ~/.bash_aliases
+echo "alias gcd='git checkout develop'" >> ~/.bash_aliases
+echo "alias gcm='git checkout master'" >> ~/.bash_aliases
