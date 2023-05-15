feca450521f5d270ba25e2e5b6ad43b59cc83786
Daniel Searle <84069850+Daniel-Searle@users.noreply.github.com>
2023-05-15 14:50:44 +0100

    Feature/VTA-1467: Investigate issues on activities service (#80)
    * feat(VTA-1467): test commit
    
    * feat(VTA-1467): test commit
    
    * feat(VTA-1467): test-i not working
    
    * feat(vta-1467): all tests passing
    
    * feat(vta-1467): undoing dependency changes
    
    * feat(vta-1467): undoing aws sdk change
    
    * feat(vta-1467): removed commented out code
    
    * feat(vta-1467): removed commented out code
    
    * feat(vta-1467): removed commented out code
    
    * feat(vta-1467): test comit
    
    * feat(vta-1467): resolve depdendency not found
    
    * feat(vta-1467): resolve depdendency not found
    
    * feat(vta-1467): resolve depdendency not found
    
    * feat(vta-1467): resolve depdendency not found
    
    * feat(vta-1467): revert
    
    * feat(vta-1467): revert

--- scripts/setUp.ts
+++ scripts/setUp.ts
@@ -1,42 +1,44 @@
-import { spawn } from 'child_process';
+import { spawn } from "child_process";
 
 // We hook to serverless offline when firing its process
-const SERVER_OK = `Offline [HTTP] listening on http://localhost:3007`;
+const SERVER_OK = `Enter "rp" to replay the last request`;
 // Serverless fires a local dynamo-db instance which is killed once the parent process is terminated
 // the current serverless script checks whether a local instance is running but does not error when binding fails
-// we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
+// we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8007
 const DYNAMO_LOCAL_ERROR_THREAD = `Exception in thread "main"`;
 
 const setupServer = (process: any) => {
   return new Promise((resolve, reject) => {
-    process.stdout.setEncoding('utf-8').on('data', (stream: any) => {
-      console.log(stream);
+    process.stdout.setEncoding("utf-8").on("data", (stream: any) => {
       if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
-    process.stderr.setEncoding('utf-8').on('data', (stream: any) => {
+    process.stderr.setEncoding("utf-8").on("data", (stream: any) => {
       if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
-        throw new Error('Internal Java process crashed');
+        throw new Error("Internal Java process crashed");
       }
-      reject(stream);
+      if (stream.includes(SERVER_OK)) {
+        resolve(process);
+      }
     });
 
-    process.on('exit', (code: any, signal: any) => {
+    process.on("exit", (code: any, signal: any) => {
       if (code !== 137) {
-        console.info(`process terminated with code: ${code} and signal: ${signal}`)
+        console.info(
+            `process terminated with code: ${code} and signal: ${signal}`
+        );
       }
-    }
-    );
+    });
   });
 };
 
-const server = spawn('npm', ['run', 'start'], {});
+const server = spawn("npm", ["run", "start"], {});
 
 module.exports = async () => {
-  console.log(`\nSetting up Integration tests...\n\n`);
   try {
+    console.log(`\nSetting up Integration tests...\n`);
     const instance = await setupServer(server);
     // @ts-ignore
     const { pid } = instance;
@@ -45,7 +47,7 @@ module.exports = async () => {
     on pid: ${pid}
     `);
   } catch (e) {
-    console.error('Something wrong happened:\n');
+    console.error("Something wrong happened:\n");
     console.error(e);
     process.exit(1);
   }
